1c8565e2b606bdc131f8b0622b1dbc8d7735070a
FlutLab.io <>
2022-06-15 09:25:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MTN'),
+          centerTitle: true,
+          backgroundColor: Colors.yellow,
+        ),
+        body: Center(
+          child: Text(
+            'WELCOME',
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 2.0,
+              color: Colors.greenAccent,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.amber,
+        ),
+      ),
+    ));
