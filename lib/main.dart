f8b058ec5406aa33d6d298588ad3fd33ea3227cd
FlutLab.io <>
2022-06-15 10:00:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MTN'),
-          centerTitle: true,
-          backgroundColor: Colors.yellow,
-        ),
-        body: Center(
-          child: Text(
-            'WELCOME',
-            style: TextStyle(
-              fontSize: 50.0,
-              fontWeight: FontWeight.w600,
-              letterSpacing: 2.0,
-              color: Colors.greenAccent,
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Colors.amber,
-        ),
-      ),
+      home: Home(),
     ));
 
 class Home extends StatelessWidget {
@@ -50,7 +28,7 @@ class Home extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
+        child: Text('click me'),
         backgroundColor: Colors.amber,
       ),
     );
