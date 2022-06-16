e879fb0dedee60b822328f636fb6b65fa17cb7f9
FlutLab.io <>
2022-06-16 20:43:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,6 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.yellow,
       ),
       body: Center(
-        child: Image(
-          image: NetworkImage('https://images.unsplash.com/photo-1655321300721-5debfd81176b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=858&q=80'),
           child: Icon(
             Icons.airpot_shuttle,
             color: Colors.brown,
