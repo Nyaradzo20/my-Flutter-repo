78125fe4e41feaed71fa05954a6c7973a2d4c146
FlutLab.io <>
2022-06-16 20:57:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MTN'),
+        title: Text('hello'),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
